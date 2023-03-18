7d5474c6670087be814f75ed23241347c49c4eb4
Daniel Robert <daniel4life1@gmail.com>
2023-03-18 22:49:12 +0100

    added ability to publish migrations

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -34,44 +34,6 @@ class InstallCommand extends Command
         $this->comment('Publishing Otp Generator Configuration...');
         $this->callSilent('vendor:publish', ['--tag' => 'otp-config']);
 
-        $this->registerTelescopeServiceProvider();
-
         $this->info('Otp Generator scaffolding installed successfully.');
-    }
-
-    /**
-     * Register the Otp service provider in the application configuration file.
-     *
-     * @return void
-     */
-    protected function registerTelescopeServiceProvider()
-    {
-        $namespace = Str::replaceLast('\\', '', $this->laravel->getNamespace());
-
-        $appConfig = file_get_contents(config_path('app.php'));
-
-        if (Str::contains($appConfig, $namespace.'\\Providers\\OtpGeneratorServiceProvider::class')) {
-            return;
-        }
-
-        $lineEndingCount = [
-            "\r\n" => substr_count($appConfig, "\r\n"),
-            "\r" => substr_count($appConfig, "\r"),
-            "\n" => substr_count($appConfig, "\n"),
-        ];
-
-        $eol = array_keys($lineEndingCount, max($lineEndingCount))[0];
-
-        file_put_contents(config_path('app.php'), str_replace(
-            "{$namespace}\\Providers\RouteServiceProvider::class,".$eol,
-            "{$namespace}\\Providers\RouteServiceProvider::class,".$eol."        {$namespace}\Providers\OtpGeneratorServiceProvider::class,".$eol,
-            $appConfig
-        ));
-
-        file_put_contents(app_path('Providers/OtpGeneratorServiceProvider.php'), str_replace(
-            "namespace App\Providers;",
-            "namespace {$namespace}\Providers;",
-            file_get_contents(app_path('Providers/OtpGeneratorServiceProvider.php'))
-        ));
     }
 }
