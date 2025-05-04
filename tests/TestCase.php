e33aefd5be290c982a941578c0ccc37080fbadfd
= <=>
2025-05-04 15:26:57 +0100

    fixed migration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace DanielRobert\Otp\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use DanielRobert\Otp\OtpGeneratorServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\TestCase as BaseTestCase;
@@ -14,14 +13,9 @@ abstract class TestCase extends BaseTestCase
     public function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'DanielRobert\\Otp\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-
         $path = realpath(__DIR__.'/../database/migrations');
-        var_dump($path, file_exists($path));
         $this->loadMigrationsFrom($path);
+        $this->artisan('migrate');
     }
 
     protected function getPackageProviders($app): array
