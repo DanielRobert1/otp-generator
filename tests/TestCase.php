79746f365dbbedca5a00cab736ee74c22ead3352
= <=>
2025-05-04 15:06:36 +0100

    workflow fixes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,7 +18,6 @@ abstract class TestCase extends BaseTestCase
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'DanielRobert\\Otp\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
-        $this->loadMigrationsFrom(realpath(__DIR__.'/../database/migrations'));
     }
 
     protected function getPackageProviders($app): array
